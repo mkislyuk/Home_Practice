4c05bf60e2fac0a3288bb99f38d895f114cbbb8a
mkislyuk <93876843+mkislyuk@users.noreply.github.com>
2022-01-19 08:52:45 -0800

    Created add method

--- src/Git_Practice/Calculations.java
+++ src/Git_Practice/Calculations.java
@@ -2,6 +2,11 @@ package Git_Practice;
 
 public class Calculations {
     public static void main(String[] args) {
-        
+
+    }
+
+    public static int add(int num1,int num2){
+        int result = num1 +num2;
+        return result;
     }
 }
